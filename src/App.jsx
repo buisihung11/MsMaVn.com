4f54b5a16fa1d8e07c6f702eb8ed67f395039a00
bshu <bshu@PF3M7F9K>
2023-02-20 14:19:21 +0700

    feat: setup font SVN-Gotham

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1
+          style={{
+            fontWeight: 'bold',
+            color: '#223368',
+          }}
+        >
+          IMPORT VIDEO TRUYỀN CẢM HỨNG
+        </h1>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      custom: {
+        families: [
+          {
+            name: 'SVN-Gotham',
+            local: 'SVN-Gotham',
+            src: './src/assets/fonts/*.otf',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
-    outDir: 'build'
-  }
+    outDir: 'build',
+  },
 })
